cdde93333fb3fbe5fb5aa9c5f21ad055acb7b15e
Kyrne <johnnylam0914@gmail.com>
2023-01-31 17:00:56 +0100

    clean up

--- src/search.py
+++ src/search.py
@@ -1,28 +1,34 @@
 from flask import Blueprint, request, render_template, session
 import require as require
+from require import response
 from flask_login import current_user
 from sql.auth import *
 from sql.inventory import *
+
 
 search_blueprint = Blueprint("search",__name__,template_folder="../templates")
 
 @require.fields(request)
 def fetch_items(search_input):
+    if not search_input:
+        return render_template("index.html", user = current_user, items=[])
     search_input = '%' + search_input + '%'
-    #TODO add search by SN and category
     items_searched = get_item_by_search_name(search_input)
-    category_groups = get_all_categories_grouped_by_supercategory()
     if items_searched is None:
         items_searched = []
-    return render_template("index.html", user = current_user, items=items_searched, category_groups=category_groups)
+    return render_template("index.html", user = current_user, items=items_searched)
     
 
 @search_blueprint.route("/search", methods =["GET", "POST"])
 def search_database():
     if request.method == "POST":
-        session["title"] = "Bolaget SEARCH"
         session["items"] = fetch_items()
-        return session["items"]
+        session["search_check"] = True
+        return response(200)
     if request.method == "GET":
-        #TODO remove session after return and make sure user is logged in
-        return session["items"]
+        # makes sure that the template returned matches searched, if the url was just typed then return empty search
+        if session["search_check"]:
+            session["search_check"] = False
+            return session["items"]
+        else:
+            return render_template("index.html", user = current_user, items = [])
