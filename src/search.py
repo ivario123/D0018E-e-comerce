0ca87839900260757609921ab0edeb97be422628
Kyrne <johnnylam0914@gmail.com>
2023-02-13 15:35:46 +0100

    search for categories

--- src/search.py
+++ src/search.py
@@ -11,13 +11,17 @@ search_blueprint = Blueprint(
 def fetch_items(search_input):
     if not search_input:
         return redirect(url_for('index'))
+    category_input = []
+    category_input.append(search_input)
+    search_category = get_all_items_with_category(category_input)
     search_input = '%' + search_input + '%'
-    items_searched = get_item_by_search_name(search_input)
-    if items_searched is None:
-        items_searched = []
-    for item in items_searched:
+    search_name = get_item_by_search_name(search_input)
+    if search_name is None:
+        search_name = []
+    search_name.extend(item for item in search_category if item not in search_name)
+    for item in search_name:
         item.add_rating(get_average_review_for(item.serial_number))
-    return render_template("index.html", user=current_user, items=items_searched)
+    return render_template("index.html", user=current_user, items=search_name)
 
 
 @search_blueprint.route("/search", methods=["GET"])
