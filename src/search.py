da0d20804e30bb1894eaabe8594cee01e6ac9f75
Kyrne <johnnylam0914@gmail.com>
2023-02-17 14:59:13 +0100

    possible to search for super categories

--- src/search.py
+++ src/search.py
@@ -16,15 +16,26 @@ def selected_categories():
     categories = loads(categories)
     return categories
 
-def filter(search_name):
+def remove_dupes(items):
+    seen = set()
+    filtered = []
+
+    for item in items:
+        if item.serial_number not in seen:
+            filtered.append(item)
+            seen.add(item.serial_number)
+
+    return filtered
+
+def filter(search_result):
     selected_cat = selected_categories()
     search_filter = get_all_items_with_category(selected_cat)
-    search_name.extend(search_filter)
+    search_result.extend(search_filter)
 
     seen = set()
     dupes = []
 
-    for item in search_name:
+    for item in search_result:
         if item.serial_number in seen:
             dupes.append(item)
         else:
@@ -32,68 +43,73 @@ def filter(search_name):
 
     return dupes
 
-def sort(search_name, method):
+def sort(search_result, method):
     if method == "price_asc":
-        return sorted(search_name, key=lambda x: x.price)
+        return sorted(search_result, key=lambda x: x.price)
     elif method == "price_dsc":
-        return sorted(search_name, key=lambda x: x.price, reverse=True)
+        return sorted(search_result, key=lambda x: x.price, reverse=True)
     elif method == "name_asc":
-        return sorted(search_name, key=lambda x: x.name)
+        return sorted(search_result, key=lambda x: x.name)
     else:
-        return sorted(search_name, key=lambda x: x.name, reverse = True) 
+        return sorted(search_result, key=lambda x: x.name, reverse = True) 
 
 def fetch_all(filter_input, method):
     category_group = super_categories_and_sub()
-    search_name = get_all_items()
+    search_result = get_all_items()
 
     # if filtering for categories
     if filter_input:
-        search_name = filter(search_name)
+        search_result = filter(search_result)
 
     if method:
-        search_name = sort(search_name, method)
+        search_result = sort(search_result, method)
     
-    for item in search_name:
+    for item in search_result:
         item.add_rating(get_average_review_for(item.serial_number))
 
-    return render_template("search.html", user=current_user, items=search_name, category_groups = category_group)
+    return render_template("search.html", user=current_user, items=search_result, category_groups = category_group)
 
 def fetch_items(search_input, filter_input, method):
-    category_input = []
-    category_input.append(search_input)
-    search_category = get_all_items_with_category(category_input)
+    search_category = get_all_items_with_category([search_input])
     search_input = f"%{search_input}%"
-    search_name = get_item_by_search_name(search_input)
-    if search_name is None:
-        search_name = []
+    search_result = get_item_by_search_name(search_input)
+    super_result = get_all_items_with_super(search_input)
 
     # group items from category search with name search
-    search_name.extend(item for item in search_category if item not in search_name)
+    search_result.extend(item for item in search_category if item not in search_result)
+    search_result.extend(item for item in super_result if item not in search_result)
 
-    # TODO only show categories of items in search result
-    # need to check categories before filtering after categories to not lose categories from pure search. 
+    # remove duplicates, seems like i get duplicates after extend even when im telling it not to include duplictaes?
+    search_result = remove_dupes(search_result)
+    
+    # If search is empty
+    if search_result == []:
+        return render_template("search.html", user=current_user, items=search_result, category_groups = [])
+
+    # Get exact categories
     serial_numbers = []
-    for item in search_name:
+    for item in search_result:
         serial_numbers.append(item.serial_number)
     exact_category = search_get_categories(serial_numbers)
 
     # if filtering for categories
     if filter_input:
-        search_name = filter(search_name)
+        search_result = filter(search_result)
 
+    # if sorting
     if method:
-        search_name = sort(search_name, method)
+        search_result = sort(search_result, method)
     
-    for item in search_name:
+    # get reviews
+    for item in search_result:
         item.add_rating(get_average_review_for(item.serial_number))
     
-    return render_template("search.html", user=current_user, items=search_name, category_groups = exact_category)
+    return render_template("search.html", user=current_user, items=search_result, category_groups = exact_category)
 
 
 @search_blueprint.route("/search", methods=["GET"])
 def search_database():
     search_input = request.args.get('q')
-    print(search_input)
     if search_input == "null":
         return fetch_all(request.args.get('categories'), request.args.get('sort'))
     else:
